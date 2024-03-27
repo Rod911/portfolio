907560dab593e67dfef7b1a748f891b836954f36
Malcolm Rodrigues <rmalcolm911@gmail.com>
2024-03-28 00:24:53 +0530

    add resume and favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,9 @@
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code:wght@100..700&display=swap" rel="stylesheet">
 
     @vite('resources/css/app.css')
+    <!-- favicon -->
+    <link rel="apple-touch-icon" sizes="64x64" href="{{asset('images/favicon-lg.png')}}">
+    <link rel="icon" type="image/png" sizes="64x64" href="{{asset('images/favicon-lg.png')}}">
 </head>
 
 <body class="dark:bg-gray-900 text-white">
@@ -148,6 +151,16 @@
                     </h2>
                 </div>
                 <div class="space-y-2 text-center">
+                    <div class="flex items-center justify-center space-x-2 text-sm text-gray-500 dark:text-gray-400">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">
+                            <path d="M3 10C3 6.22876 3 4.34315 4.17157 3.17157C5.34315 2 7.22876 2 11 2H13C16.7712 2 18.6569 2 19.8284 3.17157C21 4.34315 21 6.22876 21 10V14C21 17.7712 21 19.6569 19.8284 20.8284C18.6569 22 16.7712 22 13 22H11C7.22876 22 5.34315 22 4.17157 20.8284C3 19.6569 3 17.7712 3 14V10Z" stroke-width="1.5" />
+                            <path d="M8 10H16" stroke-width="1.5" stroke-linecap="round" />
+                            <path d="M8 14H13" stroke-width="1.5" stroke-linecap="round" />
+                        </svg>
+                        <p>
+                            <a href="{{asset('docs/Malcolm-Resume.pdf')}}">Resume</a>
+                        </p>
+                    </div>
                     <div class="flex items-center justify-center space-x-2 text-sm text-gray-500 dark:text-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">
                             <rect width="20" height="16" x="2" y="4" rx="2"></rect>
